fffea04ef39e93060073e8864495f60d9e71830b
mikehajj <mike.hajj@gmail.com>
2015-04-01 15:06:21 +0300

    updated cloud awareness documentation, added new images. updated data in soajs.utilities to meet documentation.

---
+++ deployment/cloud/prod/profiles/prod/configurations/servers.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    "prefix": "",
+    "credentials": null,
+    "servers": [
+        {
+            "host": "192.168.0.101",
+            "port": 27017
+        },
+		    {
+			    "host": "192.168.0.102",
+			    "port": 27017
+		    }
+    ]
+};
