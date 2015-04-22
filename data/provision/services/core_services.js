a0b9344290d78af9af705d563cc8f49ba800894a
mikehajj <mike.hajj@gmail.com>
2015-04-22 11:36:09 +0300

    updated services apis, updated admin group permissions

--- data/provision/services/core_services.js
+++ data/provision/services/core_services.js
@@ -1,52 +1,391 @@
 var core_services = [
-    {
-        "name": "urac",
-        "extKeyRequired": true,
-        "port": 4001,
-        "apis": []
-    },
-    {
-        "name": "oauth",
-        "extKeyRequired": true,
-        "port": 4002,
-        "apis": []
-    },
-    {
-        "name": "dashboard",
-        "extKeyRequired": true,
-        "port": 4003,
-        "apis": []
-    },
+	{
+		"name": "urac",
+		"extKeyRequired": true,
+		"port": 4001,
+		"apis": [
+			{
+				"l": "Change Email",
+				"group": "My Account",
+				'v': "/account/changeEmail"
+			},
+			{
+				"l": "Change Password",
+				"group": "My Account",
+				'v': "/account/changePassword"
+			},
+			{
+				"l": "Edit Profile",
+				"group": "My Account",
+				'v': "/account/editProfile"
+			},
+			{
+				"l": "Get User Info",
+				"group": "My Account",
+				"groupMain": true,
+				'v': "/account/getUser"
+			},
+			{
+				"l": "Add new User",
+				"group": "Administration",
+				'v': "/admin/addUser"
+			},
+			{
+				"l": "Change User Status",
+				"group": "Administration",
+				'v': "/admin/changeUserStatus"
+			},
+			{
+				"l": "Edit User Record",
+				"group": "Administration",
+				'v': "/admin/editUser"
+			},
+			{
+				"l": "Add new Group",
+				"group": "Administration",
+				'v': "/admin/group/add"
+			},
+			{
+				"l": "Add Users to Group",
+				"group": "Administration",
+				'v': "/admin/group/addUsers"
+			},
+			{
+				"l": "Delete Group",
+				"group": "Administration",
+				'v': "/admin/group/delete"
+			},
+			{
+				"l": "Edit Group",
+				"group": "Administration",
+				'v': "/admin/group/edit"
+			},
+			{
+				"l": "List Groups",
+				"group": "Administration",
+				'v': "/admin/group/list"
+			},
+			{
+				"l": "List Users",
+				"group": "Administration",
+				"groupMain": true,
+				'v': "/admin/listUsers"
+			},
+			{
+				"l": "Edit Multiple Groups",
+				"group": "Administration",
+				'v': "/admin/user/editGroups"
+			},
+			{
+				"l": "Validate Change Email",
+				"group": "Guest",
+				'v': "/changeEmail/validate"
+			},
+			{
+				"l": "Forgot Password",
+				"group": "Guest",
+				'v': "/forgotPassword"
+			},
+			{
+				"l": "Register",
+				"group": "Guest",
+				'v': "/join"
+			},
+			{
+				"l": "Validate Register",
+				"group": "Guest",
+				'v': "/join/validate"
+			},
+			{
+				"l": "Login",
+				"group": "Guest",
+				"groupMain": true,
+				'v': "/login"
+			},
+			{
+				"l": "Logout",
+				"group": "Guest",
+				'v': "/logout"
+			},
+			{
+				"l": "Reset Password",
+				"group": "Guest",
+				'v': "/resetPassword"
+			}
+		]
+	},
+	{
+		"name": "oauth",
+		"extKeyRequired": true,
+		"port": 4002,
+		"apis": [
+			{
+				"l": "Create Token",
+				'v': "/token"
+			},
+			{
+				"l": "Kill Token",
+				'v': "/kill"
+			}
+		]
+
+	},
+	{
+		"name": "dashboard",
+		"extKeyRequired": true,
+		"port": 4003,
+		"apis": [
+			{
+				'l': "List Environments",
+				'v': "/environment/list",
+				"group": "Environment",
+				"groupMain": true
+			},
+			{
+				'l': "Add Environment",
+				'v': "/environment/add",
+				"group": "Environment"
+			},
+			{
+				'l': "Delete Environment",
+				'v': "/environment/delete",
+				"group": "Environment"
+			},
+			{
+				'l': "Update Environment",
+				'v': "/environment/update",
+				"group": "Environment"
+			},
+			{
+				'l': "Update Product",
+				'v': "/product/update",
+				"group": "Product"
+			},
+			{
+				'l': "Add Product",
+				'v': "/product/add",
+				"group": "Product"
+			},
+			{
+				'l': "Delete Product",
+				'v': "/product/delete",
+				"group": "Product"
+			},
+			{
+				'l': "List Products",
+				'v': "/product/list",
+				"group": "Product",
+				"groupMain": true
+			},
+			{
+				'l': "Get Product",
+				'v': "/product/get",
+				"group": "Product"
+			},
+
+			{
+				'l': "Add Product Package",
+				'v': "/product/packages/add",
+				"group": "Product"
+			},
+			{
+				"l": "Delete Product Package",
+				"group": "Product",
+				'v': "/product/packages/delete"
+			},
+
+			{
+				"l": "List Product Packages",
+				"group": "Product",
+				'v': "/product/packages/list"
+			},
+			{
+				"l": "Update Product Package",
+				"group": "Product",
+				'v': "/product/packages/update"
+			},
+			{
+				"l": "List Tenants",
+				"group": "Tenant",
+				"groupMain": true,
+				'v': "/tenant/list"
+			},
+			{
+				"l": "Get Tenant Info",
+				"group": "Tenant",
+				'v': "/tenant/get"
+			},
+
+			{
+				"l": "Add Tenant",
+				"group": "Tenant",
+				'v': "/tenant/add"
+			},
+			{
+				"l": "Update Tenant",
+				"group": "Tenant",
+				'v': "/tenant/update"
+			},
+			{
+				"l": "List Tenant Applications",
+				"group": "Tenant Application",
+				'v': "/tenant/application/list",
+				"groupMain": true
+			},
+			{
+				"l": "Add Tenant Application",
+				"group": "Tenant Application",
+				'v': "/tenant/application/add"
+			},
+
+			{
+				"l": "Update Tenant Application",
+				"group": "Tenant Application",
+				'v': "/tenant/application/update"
+			},
+			{
+				"l": "Delete Tenant",
+				"group": "Tenant",
+				'v': "/tenant/delete"
+			},
+			{
+				"l": "Delete Tenant Application",
+				"group": "Tenant Application",
+				'v': "/tenant/application/delete"
+			},
+
+			{
+				"l": "Add Tenant Application Key",
+				"group": "Tenant Application",
+				'v': "/tenant/application/key/add"
+			},
+			{
+				"l": "List Application Key Configuration",
+				"group": "Tenant Application",
+				'v': "/tenant/application/key/config/list"
+			},
+
+			{
+				"l": "Update Application Key Configuration",
+				"group": "Tenant Application",
+				'v': "/tenant/application/key/config/update"
+			},
+
+			{
+				"l": "Delete Application Key",
+				"group": "Tenant Application",
+				'v': "/tenant/application/key/delete"
+			},
+			{
+				"l": "Add Application External Key",
+				"group": "Tenant Application",
+				'v': "/tenant/application/key/ext/add"
+			},
+			{
+				"l": "Delete Application External Key",
+				"group": "Tenant Application",
+				'v': "/tenant/application/key/ext/delete"
+			},
+			{
+				"l": "List Application External Keys",
+				"group": "Tenant Application",
+				'v': "/tenant/application/key/ext/list"
+			},
+			{
+				"l": "Update Application External Key",
+				"group": "Tenant Application",
+				'v': "/tenant/application/key/ext/update"
+			},
+			{
+				"l": "List Application Keys",
+				"group": "Tenant Application",
+				'v': "/tenant/application/key/list"
+			},
+			{
+				"l": "List Tenant oAuth Users",
+				"group": "Tenant oAuth",
+				"groupMain": true,
+				'v': "/tenant/oauth/users/list"
+			},
+			{
+				"l": "Add Tenant oAuth Configuration",
+				"group": "Tenant oAuth",
+				'v': "/tenant/oauth/add"
+			},
+			{
+				"l": "Delete Tenant oAuth Configuration",
+				"group": "Tenant oAuth",
+				'v': "/tenant/oauth/delete"
+			},
+			{
+				"l": "Get Tenant oAuth Configuration",
+				"group": "Tenant oAuth",
+				'v': "/tenant/oauth/list"
+			},
+			{
+				"l": "Update Tenant oAuth Configuration",
+				"group": "Tenant oAuth",
+				'v': "/tenant/oauth/update"
+			},
+			{
+				"l": "Add Tenant oAuth User",
+				"group": "Tenant oAuth",
+				'v': "/tenant/oauth/users/add"
+			},
+			{
+				"l": "Delete Tenant oAuth User",
+				"group": "Tenant oAuth",
+				'v': "/tenant/oauth/users/delete"
+			},
+			{
+				"l": "Update Tenant oAuth User",
+				"group": "Tenant oAuth",
+				'v': "/tenant/oauth/users/update"
+			},
+			{
+				"l": "List Hosts",
+				"group": "Hosts",
+				'v': "/hosts/list",
+				"groupMain": true
+			},
+			{
+				"l": "List Services",
+				"group": "Services",
+				'v': "/services/list",
+				"groupMain": true
+			}
+		]
+	},
 	{
 		"name": "example01",
 		"extKeyRequired": false,
 		"port": 4010,
 		"apis": []
 	},
-    {
-        "name": "example02",
-        "extKeyRequired": true,
-        "port": 4011,
-        "apis": []
-    },
+	{
+		"name": "example02",
+		"extKeyRequired": true,
+		"port": 4011,
+		"apis": []
+	},
 	{
 		"name": "example03",
 		"extKeyRequired": true,
 		"port": 4012,
 		"apis": []
 	},
-    {
-        "name": "example04",
-        "extKeyRequired": true,
-        "port": 4013,
-        "apis": []
-    }
+	{
+		"name": "example04",
+		"extKeyRequired": true,
+		"port": 4013,
+		"apis": []
+	}
 ];
 
 var core_hosts = [
-    {
-        "env": "dev",
-        "name": "tony",
-        "ip": "127.0.0.1"
-    }
+	{
+		"env": "dev",
+		"name": "tony",
+		"ip": "127.0.0.1"
+	}
 ];
--- data/provision/urac/groups/administrator.js
+++ data/provision/urac/groups/administrator.js
@@ -1,16 +1,19 @@
 'use strict';
 
 var administrator = {
-	"_id": ObjectId('55128764e603d7e01ab1688f'), 
-	"locked" : true,
+	"_id": ObjectId('55128764e603d7e01ab1688f'),
+	"locked": true,
 	"code": "administrator",
-	"name": "administrator",	 
+	"name": "administrator",
 	"description": "this is the administrator group",
-	"permissions":["members",
-					"environments",
-					"productization",
-					"productization_packages",
-					"multi-tenancy",
-					"multi-tenancy_applications",
-					"multi-tenancy_keys" ] // to union with tenant permissions on login
+	"permissions": [
+		"members",
+		"environments",
+		"services",
+		"productization",
+		"productization_packages", "product-acl",
+		"multi-tenancy", "tenant-app-acl",
+		"multi-tenancy_applications",
+		"multi-tenancy_keys"
+	] // to union with tenant permissions on login
 };
