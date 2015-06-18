c8d20b444805b4cd8770afc413bdd46fd37d0ec9
mikehajj <mike.hajj@gmail.com>
2015-06-18 18:08:47 +0300

    removed _TTL and expDates from tenants applications and extKeys

--- data/provision/tenants/owner.js
+++ data/provision/tenants/owner.js
@@ -12,13 +12,11 @@ var dsbrd = {
 			"package": "DSBRD_MAIN",
 			"appId": ObjectId('5512926a7a1f0e2123f638de'),
 			"description": "This application uses the Dashboard Main Package.",
-			"_TTL": 7 * 24 * 3600 * 1000, // 7 days hours
 			"keys": [
 				{
 					"key": "38145c67717c73d3febd16df38abf311",
 					"extKeys": [
 						{
-							"expDate": new Date().getTime() + 7 * 24 * 3600 * 1000, // + 7 days
 							"extKey": "9b96ba56ce934ded56c3f21ac9bdaddc8ba4782b7753cf07576bfabcace8632eba1749ff1187239ef1f56dd74377aa1e5d0a1113de2ed18368af4b808ad245bc7da986e101caddb7b75992b14d6a866db884ea8aee5ab02786886ecf9f25e974",
 							"device": null,
 							"geo": null
--- data/provision/tenants/test.js
+++ data/provision/tenants/test.js
@@ -21,13 +21,11 @@ var test = {
 				"oauth": {},
 				"dashboard": {}
 			},
-			"_TTL": 7 * 24 * 3600 * 1000, // 7 days hours
 			"keys": [
 				{
 					"key": "d1eaaf5fdc35c11119330a8a0273fee9",
 					"extKeys": [
 						{
-							"expDate": new Date().getTime() + 7 * 24 * 3600 * 1000, // + 7 days
 							"extKey": "aa39b5490c4a4ed0e56d7ec1232a428f771e8bb83cfcee16de14f735d0f5da587d5968ec4f785e38570902fd24e0b522b46cb171872d1ea038e88328e7d973ff47d9392f72b2d49566209eb88eb60aed8534a965cf30072c39565bd8d72f68ac",
 							"device": {},
 							"geo": {}
@@ -91,13 +89,11 @@ var test = {
 				"urac": {},
 				"example03": {}
 			},
-			"_TTL": 5 * 24 * 3600 * 1000,
 			"keys": [
 				{
 					"key": "695d3456de70fddc9e1e60a6d85b97d3",
 					"extKeys": [
 						{
-							"expDate": new Date().getTime() + 86400000,
 							"extKey": "aa39b5490c4a4ed0e56d7ec1232a428f7ad78ebb7347db3fc9875cb10c2bce39bbf8aabacf9e00420afb580b15698c04ce10d659d1972ebc53e76b6bbae0c113bee1e23062800bc830e4c329ca913fefebd1f1222295cf2eb5486224044b4d0c",
 							"device": {},
 							"geo": {}
@@ -121,13 +117,11 @@ var test = {
 			"package": "TPROD_EX03",
 			"appId": ObjectId("30d2cb5fc04ce51e06000003"),
 			"description": "This application uses Example03 Package of Test Product.",
-			"_TTL": 24 * 3600 * 1000, // 24 hours
 			"keys": [
 				{
 					"key": "ff7b65bb252201121f1be95adc08f44a",
 					"extKeys": [
 						{
-							"expDate": new Date().getTime() + 86400000,
 							"extKey": "aa39b5490c4a4ed0e56d7ec1232a428f1c5b5dcabc0788ce563402e233386738fc3eb18234a486ce1667cf70bd0e8b08890a86126cf1aa8d38f84606d8a6346359a61678428343e01319e0b784bc7e2ca267bbaafccffcb6174206e8c83f2a25",
 							"device": {},
 							"geo": {}
