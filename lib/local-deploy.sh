046b1332923308e70c7119eba4ca56cd5cc1dc56
antoinehage <me@antoinehage.com>
2015-07-28 18:53:09 -0400

    simplified deploy local

--- lib/local-deploy.sh
+++ lib/local-deploy.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
+DATA_CONTAINER='soajsData'
 IMAGE_PREFIX='antoinehage'
-PROFILE='/opt/soajs/FILES/profiles/single.js'
+
+function createContainer(){
+    local WHAT=${1}
+    local ENV='-e NODE_ENV=production -e SOAJS_ENV=dashboard -e SOAJS_PROFILE=/opt/soajs/FILES/profiles/single.js -e SOAJS_SRV_AUTOREGISTERHOST=true'
+    local VLM='-v /Users/soajs/FILES:/opt/soajs/FILES -v /Users/soajs/open_source/services/'${WHAT}':/opt/soajs/node_modules/'${WHAT}''
+
+    echo $'\n- Starting Controller Container '${WHAT}' ...'
+
+    if [ ${WHAT} == "dashboard" ]; then
+        local EXTRA='-e "SOAJS_ENV_WORKDIR=/Users/" -v /Users/soajs:/Users/soajs -v /var/run/docker.sock:/var/run/docker.sock'
+        docker run -d --link ${DATA_CONTAINER}:dataProxy01 ${ENV} ${VLM} ${EXTRA} -i -t --name ${WHAT} ${IMAGE_PREFIX}/soajs bash -c 'cd /opt/soajs/node_modules/'${WHAT}'/; npm install; /opt/soajs/FILES/scripts/runService.sh /opt/soajs/node_modules/'${WHAT}/'index.js'
+    else
+        docker run -d --link ${DATA_CONTAINER}:dataProxy01 ${ENV} ${VLM} -i -t --name ${WHAT} ${IMAGE_PREFIX}/soajs bash -c 'cd /opt/soajs/node_modules/'${WHAT}'/; npm install; /opt/soajs/FILES/scripts/runService.sh /opt/soajs/node_modules/'${WHAT}'/index.js'
+    fi
+
+    echo $'\n--------------------------'
+}
 
 ###################################
-#Terminal 1:
 #SOAJSDATA container
 ###################################
 echo "0- Building /data folder inside VM"
@@ -16,7 +32,7 @@ docker rm $(docker ps -a -q)
 echo $'\n--------------------------'
 
 echo $'\n2- Starging Mongo Container "soajsData" ...'
-docker run -d -p 27017:27017 -v /data:/data -v /data/db:/data/db --name soajsData mongo mongod --smallfiles
+docker run -d -p 27017:27017 -v /data:/data -v /data/db:/data/db --name ${DATA_CONTAINER} mongo mongod --smallfiles
 echo $'\n--------------------------'
 
 #get mongo container IP address
@@ -31,37 +47,33 @@ echo $'\n4- Importing URAC data...'
 node index data import urac ${MONGOIP}
 echo $'\n--------------------------'
 
+
+echo $'\n5- Starting SERVICES ...'
 ###################################
-#Terminal 2:
 #URAC container
 ###################################
-echo $'\n5- Starting URAC Container "urac" ... '
-docker run -d --link soajsData:dataProxy01 -e "NODE_ENV=production" -e "SOAJS_ENV=dashboard" -e "SOAJS_PROFILE=/opt/soajs/FILES/profiles/single.js" -e "SOAJS_SRV_AUTOREGISTERHOST=true" -v /Users/soajs/FILES:/opt/soajs/FILES -v /Users/soajs/open_source/services/urac:/opt/soajs/node_modules/urac -i -t --name urac ${IMAGE_PREFIX}/soajs bash -c 'cd /opt/soajs/node_modules/urac/; npm install; /opt/soajs/FILES/scripts/runService.sh /opt/soajs/node_modules/urac/index.js'
-echo $'\n--------------------------'
+createContainer "urac"
+
 ###################################
-#Terminal 3:
 #DASHBOARD container
 ###################################
-echo $'\n6- Starting Dashboard Container "dashboard" ... '
-docker run -d --link soajsData:dataProxy01 -e "NODE_ENV=production" -e "SOAJS_ENV=dashboard" -e "SOAJS_PROFILE=/opt/soajs/FILES/profiles/single.js" -e "SOAJS_SRV_AUTOREGISTERHOST=true" -e "SOAJS_ENV_WORKDIR=/Users/" -v /Users/soajs/open_source/services/dashboard:/opt/soajs/node_modules/dashboard -v /Users/soajs/FILES:/opt/soajs/FILES -v /Users/soajs:/Users/soajs -v /var/run/docker.sock:/var/run/docker.sock -i -t --name dashboard ${IMAGE_PREFIX}/soajs bash -c 'cd /opt/soajs/node_modules/dashboard/; npm install; /opt/soajs/FILES/scripts/runService.sh /opt/soajs/node_modules/dashboard/index.js'
-echo $'\n--------------------------'
+createContainer "dashboard"
+
 ###################################
-#Terminal 4:
 #CONTROLLER container
 ###################################
 sleep 5
-echo $'\n7- Starting Controller Container "controller" ...'
-docker run -d --link soajsData:dataProxy01 -e "SOAJS_ENV=dashboard" -e "NODE_ENV=production" -e "SOAJS_PROFILE=/opt/soajs/FILES/profiles/single.js" -e "SOAJS_SRV_AUTOREGISTERHOST=true" -v /Users/soajs/FILES:/opt/soajs/FILES -v /Users/soajs/open_source/services/controller:/opt/soajs/node_modules/controller -i -t --name controller ${IMAGE_PREFIX}/soajs bash -c 'cd /opt/soajs/node_modules/controller/; npm install; /opt/soajs/FILES/scripts/runService.sh /opt/soajs/node_modules/controller/index.js'
-echo $'\n--------------------------'
+createContainer "controller"
+
 ###################################
-#Terminal 5:
 #NGINX container
 ###################################
 sleep 5
-echo $'\n8- Starting NGINX Container "nginx" ... '
-docker run -d --link controller:controllerProxy01 -p 80:80 -e "SOAJS_NX_NBCONTROLLER=1" -e "SOAJS_NX_APIDOMAIN=dashboard-api.soajs.org" -e "SOAJS_NX_DASHDOMAIN=dashboard.soajs.org" -e "SOAJS_NX_APIPORT=80" -v /Users/soajs/open_source/dashboard:/opt/soajs/open_source/dashboard/ --name nginx ${IMAGE_PREFIX}/nginx
+echo $'\n6- Starting NGINX Container "nginx" ... '
+docker run -d --link controller:controllerProxy01 -p 80:80 -e "SOAJS_NX_NBCONTROLLER=1" -e "SOAJS_NX_APIDOMAIN=dashboard-api.soajs.org" -e "SOAJS_NX_DASHDOMAIN=dashboard.soajs.org" -e "SOAJS_NX_APIPORT=80" -v /Users/soajs/open_source/dashboard:/opt/soajs/open_source/dashboard/ -v /Users/soajs/FILES:/opt/soajs/FILES --name nginx ${IMAGE_PREFIX}/nginx bash -c '/opt/soajs/FILES/scripts/runNginx.sh'
 echo $'\n--------------------------'
 
-echo $'\n9- Containers created and deployed:'
+echo $'\n7- Containers created and deployed:'
 docker ps
 echo $'\n 5 containers started, please login to the dashboard @ http://dashboard.soajs.org'
+
