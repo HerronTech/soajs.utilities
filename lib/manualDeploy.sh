00ee006fc61a3edce31f7444dba7ba8b428c78cc
antoinehage <me@antoinehage.com>
2015-07-30 19:44:41 -0400

    enhanced manual deploy

--- lib/manualDeploy.sh
+++ lib/manualDeploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+WRK_DIR='/opt/tmp/soajs/node_modules'
 
 function program_is_installed {
   # set to 1 initially
@@ -39,7 +40,7 @@ fi
 mongo --eval "db.stats()"
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-    echo $'\n ...mongodb not running'
+    echo $'\n ...mongodb not running. PLease start mongodb!'
     exit -1
 else
     echo $'\n mongodb running! continue...'
@@ -51,6 +52,72 @@ echo $'\n2- Importing URAC data...'
 node index data import urac
 echo $'\n--------------------------'
 
+mkdir -p ${WRK_DIR}
+pushd ${WRK_DIR}
 
+function startDashboard()
+{
+    pushd soajs.controller
+    node . &
+    popd
+    pushd soajs.urac
+    node . &
+    popd
+    pushd soajs.dashboard
+    node . &
+    popd
 
-echo "DONE"
+    ps aux | grep node
+    echo "DONE"
+}
+
+function uracSuccess()
+{
+    npm install soajs.oauth
+    npm install soajs.GCS
+    npm install soajs.examples
+    startDashboard
+}
+function uracFailure()
+{
+    echo $'\n ... unable to install urac npm package. exiting!'
+    exit -1
+}
+function dashSuccess()
+{
+    npm install soajs.urac
+    b=$!
+    wait $b && uracSuccess || uracFailure
+}
+function dashFailure()
+{
+    echo $'\n ... unable to install dashboard npm package. exiting!'
+    exit -1
+}
+function controllerSuccess()
+{
+    npm install soajs.dashboard
+    b=$!
+    wait $b && dashSuccess || dashFailure
+}
+function controllerFailure()
+{
+    echo $'\n ... unable to install controller npm package. exiting!'
+    exit -1
+}
+function soajsSuccess()
+{
+    npm install soajs.controller
+    b=$!
+    wait $b && controllerSuccess || controllerFailure
+}
+function soajsFailure()
+{
+    echo $'\n ... unable to install soajs npm package. exiting!'
+    exit -1
+}
+
+export NODE_ENV=production
+npm install soajs
+b=$!
+wait $b && soajsSuccess || soajsFailure
