0aa8973ed946852c381aa8400612a00e4fce5ea0
antoinehage <me@antoinehage.com>
2015-12-26 11:31:33 -0500

    added env to acl

--- data/provision/products/dsbrd.js
+++ data/provision/products/dsbrd.js
@@ -14,38 +14,40 @@ var dsbrdProduct = {
 			"description": "This package allows you to login to the dashboard.",
 			"acl": {
 				"dashboard": {
-					"apisPermission": "restricted",
-					"apis": {
-						"/key/get": { "access": true },
-						"/permissions/get": { "access": true },
-						"/tenant/acl/get": { "access": true }
-					}
-				},
-				"urac": {
-					"apisPermission": "restricted",
-					"access": false,
-					"apis": {
-						"/login": {},
-						"/forgotPassword": {},
-						"/changeEmail/validate": {},
-						"/join": {},
-						"/join/validate": {},
-						"/resetPassword": {},
-						"/account/getUser": {"access": true},
-						"/account/changePassword": {"access": true},
-						"/account/editProfile": {"access": true},
-						"/account/changeEmail": {"access": true},
-						"/logout": {"access": true},
-						"/admin/getUser": {"access": ["administrator", "owner"]},
-						"/admin/listUsers": {"access": ["administrator", "owner"]},
-						"/admin/addUser": {"access": ["administrator", "owner"]},
-						"/admin/changeUserStatus": {"access": ["administrator", "owner"]},
-						"/admin/editUser": {"access": ["administrator", "owner"]},
-						"/admin/group/add": {"access": ["administrator", "owner"]},
-						"/admin/group/addUsers": {"access": ["administrator", "owner"]},
-						"/admin/group/delete": {"access": ["administrator", "owner"]},
-						"/admin/group/edit": {"access": ["administrator", "owner"]},
-						"/admin/group/list": {"access": ["administrator", "owner"]}
+					"dashboard": {
+						"apisPermission": "restricted",
+						"apis": {
+							"/key/get": {"access": true},
+							"/permissions/get": {"access": true},
+							"/tenant/acl/get": {"access": true}
+						}
+					},
+					"urac": {
+						"apisPermission": "restricted",
+						"access": false,
+						"apis": {
+							"/login": {},
+							"/forgotPassword": {},
+							"/changeEmail/validate": {},
+							"/join": {},
+							"/join/validate": {},
+							"/resetPassword": {},
+							"/account/getUser": {"access": true},
+							"/account/changePassword": {"access": true},
+							"/account/editProfile": {"access": true},
+							"/account/changeEmail": {"access": true},
+							"/logout": {"access": true},
+							"/admin/getUser": {"access": ["administrator", "owner"]},
+							"/admin/listUsers": {"access": ["administrator", "owner"]},
+							"/admin/addUser": {"access": ["administrator", "owner"]},
+							"/admin/changeUserStatus": {"access": ["administrator", "owner"]},
+							"/admin/editUser": {"access": ["administrator", "owner"]},
+							"/admin/group/add": {"access": ["administrator", "owner"]},
+							"/admin/group/addUsers": {"access": ["administrator", "owner"]},
+							"/admin/group/delete": {"access": ["administrator", "owner"]},
+							"/admin/group/edit": {"access": ["administrator", "owner"]},
+							"/admin/group/list": {"access": ["administrator", "owner"]}
+						}
 					}
 				}
 			},
@@ -57,25 +59,27 @@ var dsbrdProduct = {
 			"description": "This package provides full access to manage the dashboard and urac features.",
 			"locked": true,
 			"acl":{
-				"urac": {
-					"access": false,
-					"apisRegExp": [
-						{
-							"regExp": /^\/account\/.+$/, //All APIs starting with /account/...
-							"access": true
-						},
-						{
-							"regExp": /^\/admin\/.+$/, //All APIs starting with /admin/...
-							"access": ["owner"]
-						}
-					]
-				},
 				"dashboard": {
-					"access": ["owner"]
-				},
-				"gc_articles":{
-					"apis": {},
-					"access": true
+					"urac": {
+						"access": false,
+						"apisRegExp": [
+							{
+								"regExp": /^\/account\/.+$/, //All APIs starting with /account/...
+								"access": true
+							},
+							{
+								"regExp": /^\/admin\/.+$/, //All APIs starting with /admin/...
+								"access": ["owner"]
+							}
+						]
+					},
+					"dashboard": {
+						"access": ["owner"]
+					},
+					"gc_articles": {
+						"apis": {},
+						"access": true
+					}
 				}
 			},
 			"_TTL": 7 * 24 * 3600 * 1000 // 7 days hours
@@ -86,100 +90,102 @@ var dsbrdProduct = {
 			"description": "This package provides access to the dashboard client members.",
 			"locked": true,
 			"acl":{
-				"urac": {
-					"apisPermission": "restricted",
-					"access": false,
-					"apis": {
-						"/account/getUser": {"access": true},
-						"/account/changePassword": {"access": true},
-						"/account/editProfile": {"access": true},
-						"/account/changeEmail": {"access": true},
-						"/logout": {"access": true},
-						"/changeEmail/validate": {"access": true},
-						"/admin/getUser": {"access": ["administrator"]},
-						"/admin/listUsers": {"access": ["administrator"]},
-						"/admin/addUser": {"access": ["administrator"]},
-						"/admin/changeUserStatus": {"access": ["administrator"]},
-						"/admin/editUser": {"access": ["administrator"]},
-						"/admin/group/add": {"access": ["administrator"]},
-						"/admin/group/addUsers": {"access": ["administrator"]},
-						"/admin/group/delete": {"access": ["administrator"]},
-						"/admin/group/edit": {"access": ["administrator"]},
-						"/admin/group/list": {"access": ["administrator"]}
-					}
-				},
 				"dashboard": {
-					"apisPermission": "restricted",
-					"access": ["administrator"],
-					"apis": {
-						"/tenant/acl/get": {
-							"access": ["administrator"]
-						},
-                        "/extKey/get": {
-                            "access": true
-                        },
-						"/permissions/get": {
-							"access": true
-						},
-						"/settings/tenant/get": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/update": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/oauth/list": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/oauth/delete": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/oauth/add": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/oauth/update": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/oauth/users/list": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/oauth/users/add": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/oauth/users/delete": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/oauth/users/update": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/list": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/key/add": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/key/delete": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/key/list": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/key/config/list": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/key/config/update": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/key/ext/add": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/key/ext/delete": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/key/ext/list": {
-							"access": ["administrator"]
-						},
-						"/settings/tenant/application/key/ext/update": {
-							"access": ["administrator"]
+					"urac": {
+						"apisPermission": "restricted",
+						"access": false,
+						"apis": {
+							"/account/getUser": {"access": true},
+							"/account/changePassword": {"access": true},
+							"/account/editProfile": {"access": true},
+							"/account/changeEmail": {"access": true},
+							"/logout": {"access": true},
+							"/changeEmail/validate": {"access": true},
+							"/admin/getUser": {"access": ["administrator"]},
+							"/admin/listUsers": {"access": ["administrator"]},
+							"/admin/addUser": {"access": ["administrator"]},
+							"/admin/changeUserStatus": {"access": ["administrator"]},
+							"/admin/editUser": {"access": ["administrator"]},
+							"/admin/group/add": {"access": ["administrator"]},
+							"/admin/group/addUsers": {"access": ["administrator"]},
+							"/admin/group/delete": {"access": ["administrator"]},
+							"/admin/group/edit": {"access": ["administrator"]},
+							"/admin/group/list": {"access": ["administrator"]}
+						}
+					},
+					"dashboard": {
+						"apisPermission": "restricted",
+						"access": ["administrator"],
+						"apis": {
+							"/tenant/acl/get": {
+								"access": ["administrator"]
+							},
+							"/extKey/get": {
+								"access": true
+							},
+							"/permissions/get": {
+								"access": true
+							},
+							"/settings/tenant/get": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/update": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/oauth/list": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/oauth/delete": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/oauth/add": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/oauth/update": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/oauth/users/list": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/oauth/users/add": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/oauth/users/delete": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/oauth/users/update": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/list": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/key/add": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/key/delete": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/key/list": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/key/config/list": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/key/config/update": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/key/ext/add": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/key/ext/delete": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/key/ext/list": {
+								"access": ["administrator"]
+							},
+							"/settings/tenant/application/key/ext/update": {
+								"access": ["administrator"]
+							}
 						}
 					}
 				}
