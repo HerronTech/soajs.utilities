452177778d4d03754ed0b88122c4752ee9a91b44
antoinehage <me@antoinehage.com>
2016-05-31 16:37:34 -0400

    added the ability to prompt to the user to enter mongo info @ import script

--- data/startup/import.sh
+++ data/startup/import.sh
@@ -1,9 +1,50 @@
 #!/bin/bash
 
-MONGO_URL='127.0.0.1'
-MONGO_PORT=27017
+MONGO_URL='iad1-mongos0.objectrocket.com'
+MONGO_PORT=16067
 MONGO_USERNAME=''
 MONGO_PASSWORD=''
+
+
+function readMongoInfo(){
+    echo $'\n PLease enter MongoDB Host. Default [iad1-mongos0.objectrocket.com]:'
+    read URL
+    if [ -n "${URL}" ]; then
+        MONGO_URL = ${URL}
+    fi
+    echo $'\n PLease enter MongoDB Port. Default [16067]:'
+    read PORT
+    if [ -n "${PORT}" ]; then
+        MONGO_PORT = ${PORT}
+    fi
+    echo $'\n PLease enter MongoDB Username. Default [NULL]:'
+    read UNAME
+    if [ -n "${UNAME}" ]; then
+        MONGO_USERNAME = ${UNAME}
+    fi
+    echo $'\n PLease enter MongoDB Password. Default [NULL]:'
+    read PWD
+    if [ -n "${PWD}" ]; then
+        MONGO_PASSWORD = ${PWD}
+    fi
+
+    echo $'\n Your mongo info are:'
+    echo $'\t HOST: '${MONGO_URL}
+    echo $'\t PORT: '${MONGO_PORT}
+    echo $'\t USERNAME: '${MONGO_USERNAME}
+    echo $'\t PASSWORD: '${MONGO_PASSWORD}
+
+    while true; do
+        read -p "Do you wish to continue with the above Mongo Info ([n] to go back and change)? " yn
+        case $yn in
+            [Yy]* ) echo $'\n ..... Continue'; break;;
+            [Nn]* ) readMongoInfo; break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+readMongoInfo
 
 CMD1=${MONGO_URL}':'${MONGO_PORT}/'core_provision'
 CMD2=${MONGO_URL}':'${MONGO_PORT}/'DBTN_urac'
