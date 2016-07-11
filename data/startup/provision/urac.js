b3f0ada3127bffbe5e6955b80a867516865ddf95
mikehajj <mike.hajj@gmail.com>
2016-07-11 13:25:25 +0300

    updated urac collections indexing.

--- data/startup/provision/urac.js
+++ data/startup/provision/urac.js
@@ -28,3 +28,25 @@ files.forEach(function(oneFile){
 records = [];
 records.push(owner);
 ddb.groups.insert(records);
+
+//users
+ddb.ensureIndex("users", {username: 1}, { unique: true });
+ddb.ensureIndex("users", {email: 1}, { unique: true });
+ddb.ensureIndex("users", {username: 1, status: 1});
+ddb.ensureIndex("users", {email: 1, status: 1});
+ddb.ensureIndex("users", {groups: 1, 'tenant.id': 1});
+ddb.ensureIndex("users", {username: 1, 'tenant.id': 1});
+ddb.ensureIndex("users", {status: 1});
+ddb.ensureIndex("users", {locked: 1});
+ddb.ensureIndex("users", {'tenant.id': 1});
+
+//groups
+ddb.ensureIndex("groups", {code: 1, 'tenant.id': 1});
+ddb.ensureIndex("groups", {code: 1});
+ddb.ensureIndex("groups", {'tenant.id': 1});
+ddb.ensureIndex("groups", {locked: 1});
+
+//tokens
+ddb.ensureIndex("tokens", {token: 1}, { unique: true});
+ddb.ensureIndex("tokens", {userId: 1, service: 1, status: 1});
+ddb.ensureIndex("tokens", {token: 1, service: 1, status: 1});
--- data/urac/data.js
+++ data/urac/data.js
@@ -20,7 +20,27 @@ records.push(user2);
 records.push(user1);
 ddb.users.insert(records);
 
+//users
+ddb.ensureIndex("users", {username: 1}, { unique: true });
+ddb.ensureIndex("users", {email: 1}, { unique: true });
+ddb.ensureIndex("users", {username: 1, status: 1});
+ddb.ensureIndex("users", {email: 1, status: 1});
+ddb.ensureIndex("users", {groups: 1, 'tenant.id': 1});
+ddb.ensureIndex("users", {username: 1, 'tenant.id': 1});
+ddb.ensureIndex("users", {status: 1});
+ddb.ensureIndex("users", {locked: 1});
+ddb.ensureIndex("users", {'tenant.id': 1});
 
+//groups
+ddb.ensureIndex("groups", {code: 1, 'tenant.id': 1});
+ddb.ensureIndex("groups", {code: 1});
+ddb.ensureIndex("groups", {'tenant.id': 1});
+ddb.ensureIndex("groups", {locked: 1});
+
+//tokens
+ddb.ensureIndex("tokens", {token: 1}, { unique: true});
+ddb.ensureIndex("tokens", {userId: 1, service: 1, status: 1});
+ddb.ensureIndex("tokens", {token: 1, service: 1, status: 1});
 
 var ddbT = db.getSiblingDB('TNT1_urac');
 ddbT.dropDatabase();
@@ -37,3 +57,25 @@ records.push(user1);
 records.push(user3);
 records.push(user2);
 ddbT.users.insert(records);
+
+//users
+ddbT.ensureIndex("users", {username: 1}, { unique: true });
+ddbT.ensureIndex("users", {email: 1}, { unique: true });
+ddbT.ensureIndex("users", {username: 1, status: 1});
+ddbT.ensureIndex("users", {email: 1, status: 1});
+ddbT.ensureIndex("users", {groups: 1, 'tenant.id': 1});
+ddbT.ensureIndex("users", {username: 1, 'tenant.id': 1});
+ddbT.ensureIndex("users", {status: 1});
+ddbT.ensureIndex("users", {locked: 1});
+ddbT.ensureIndex("users", {'tenant.id': 1});
+
+//groups
+ddbT.ensureIndex("groups", {code: 1, 'tenant.id': 1});
+ddbT.ensureIndex("groups", {code: 1});
+ddbT.ensureIndex("groups", {'tenant.id': 1});
+ddbT.ensureIndex("groups", {locked: 1});
+
+//tokens
+ddbT.ensureIndex("tokens", {token: 1}, { unique: true});
+ddbT.ensureIndex("tokens", {userId: 1, service: 1, status: 1});
+ddbT.ensureIndex("tokens", {token: 1, service: 1, status: 1});
